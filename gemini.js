8c3d1a7f9719ece8bcf027b3511cc0be15249b08
Chan <wrallee@naver.com>
2024-07-08 01:13:49 +0900

    feat: works-api access-token 받아오는 로직 구현

--- gemini.js
+++ gemini.js
@@ -2,12 +2,11 @@ const {GoogleGenerativeAI} = require("@google/generative-ai");
 const express = require('express');
 const dotenv = require('dotenv');
 const axios = require('axios');
-const {getJsonWebToken} = require("./jwt-utils");
+const {verifyRequestSignature, getWorksApiAccessToken} = require("./jwt-utils");
 
 dotenv.config();
 
 const BOT_ID = process.env.BOT_ID;
-const BOT_SECRET = process.env.BOT_SECRET;
 const GENERATIVE_AI_API_KEY = process.env.GENERATIVE_AI_API_KEY;
 
 const app = express();
@@ -17,27 +16,31 @@ const genAI = new GoogleGenerativeAI(GENERATIVE_AI_API_KEY);
 const model = genAI.getGenerativeModel({model: "gemini-1.5-flash"});
 
 app.use(express.json());
-
+app.use(verifyRequestSignature)
 app.post('/api/chat', async (req, res) => {
-    const {prompt} = req.body;
+    const prompt = req.body.content.text;
+    const channelId = req.body.source.channelId;
 
+    // get ai reply
     const result = await model.generateContent(prompt);
-    const aiResponse = await result.response;
+    const aiMessage = await result.response;
 
-    const responseMessage = {
+    // send chat to user-channel
+    const botApiUrl = `https://www.worksapis.com/v1.0/bots/${BOT_ID}/channels/${channelId}/messages`
+    const authToken = await getWorksApiAccessToken(req, res);
+    const botMessage = {
         content: {
             type: 'text',
-            text: aiResponse.text() // 2000자 제한 처리 필요
+            text: aiMessage.text() // 2000자 제한 처리 필요
+            // text: prompt // 2000자 제한 처리 필요
         }
     };
 
-    const botApiUrl = `https://www.worksapis.com/v1.0/bots/${BOT_ID}/channels/${channelId}/messages`
-
     try {
-        const response = await axios.post(botApiUrl, responseMessage, {
+        const response = await axios.post(botApiUrl, botMessage, {
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': `Bearer ${BOT_SECRET}`
+                'Authorization': `Bearer ${authToken}`
             }
         });
         res.status(200).send('Message sent');
--- jwt-utils.js
+++ jwt-utils.js
@@ -2,13 +2,63 @@ const jwt = require('jsonwebtoken');
 const fs = require('fs');
 const path = require('path');
 const dotenv = require('dotenv');
+const crypto = require('crypto');
+const axios = require("axios");
 
 dotenv.config();
 
+const BOT_SECRET = process.env.BOT_SECRET;
 const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_ACCOUNT = process.env.CLIENT_ACCOUNT;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const SERVICE_ACCOUNT = process.env.SERVICE_ACCOUNT;
 
-function getJsonWebToken() {
+function verifyRequestSignature(req, res, next) {
+    // 요청 본문을 문자열로 변환
+    const requestBody = JSON.stringify(req.body);
+
+    // HMAC-SHA256 생성
+    const hmac = crypto.createHmac('sha256', BOT_SECRET);
+    hmac.update(requestBody);
+    const hash = hmac.digest('base64');
+
+    // 요청 헤더에서 XXX_MY_HEADER 값 가져오기 (대소문자 구분 없이)
+    const headerKey = Object.keys(req.headers)
+        .find(key => key.toLowerCase() === 'x-works-signature');
+    const headerValue = req.headers[headerKey];
+
+    // HMAC 결과와 헤더 값 비교
+    if (hash === headerValue) {
+        next();
+    } else {
+        res.status(401).send('Invalid Request');
+    }
+}
+
+async function getWorksApiAccessToken(req, res) {
+    try {
+        const oAuthUrl = 'https://auth.worksmobile.com/oauth2/v2.0/token';
+        const jsonWebToken = encodeJsonWebToken();
+        const data = new URLSearchParams();
+        data.append('grant_type', 'urn:ietf:params:oauth:grant-type:jwt-bearer');
+        data.append('client_id', CLIENT_ID);
+        data.append('client_secret', CLIENT_SECRET);
+        data.append('scope', 'bot.message');
+        data.append('assertion', jsonWebToken);
+
+        const response = await axios.post(oAuthUrl, data, {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
+        // return response.data // TODO check refresh_token, etc...
+        return response.data.access_token;
+    } catch (error) {
+        console.error('Error sending message:', error);
+        res.status(500).send('Internal Server Error');
+    }
+}
+
+function encodeJsonWebToken() {
     // header
     const header = {
         alg: 'RS256',
@@ -18,7 +68,7 @@ function getJsonWebToken() {
     // json claim
     const payload = {
         iss: CLIENT_ID,
-        sub: CLIENT_ACCOUNT
+        sub: SERVICE_ACCOUNT
     };
 
     // signature
@@ -29,4 +79,4 @@ function getJsonWebToken() {
     return jwt.sign(payload, privateKey, {algorithm: 'RS256', expiresIn: '1h'});
 }
 
-module.exports = {getJsonWebToken}
+module.exports = {verifyRequestSignature, getWorksApiAccessToken}
